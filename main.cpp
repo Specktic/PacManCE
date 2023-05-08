c76320ab24af2aa9258c5e4b9e7b831cecada34a
Specktic <mavic030603@gmail.com>
2023-05-07 19:53:58 -0600

    add map, player, tile, tiles and window classes

--- main.cpp
+++ main.cpp
@@ -1,32 +1,5 @@
 #include <SFML/Graphics.hpp>
-
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(1600, 900), "nom nom nom");
-    window.setFramerateLimit(30);
-
-    sf::CircleShape shape(200.f);
-    shape.setFillColor(sf::Color::Blue);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        //Update window
-
-
-        //Draw on window
-
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    return 0;
-}
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
+#include <string>
---
+++ player.hpp
@@ -0,0 +1,12 @@
+#include <SFML/Graphics.hpp>
+#include <string>
+#include "map.hpp"
+
+class Player
+{
+    sf::Sprite sprite;
+    int posX;
+    int posY;
+    int lives;
+
+};
