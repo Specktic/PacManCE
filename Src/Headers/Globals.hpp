42cec76db0d539a4ed512b6f1f5232b5bcbf0e11
Specktic <mavic030603@gmail.com>
2023-05-09 22:24:19 -0600

    render a window define some global variables

---
+++ Src/Headers/Globals.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+//Ghosts moving speed
+constexpr unsigned char GHOST_SPEED = 1;
+
+//Map height
+constexpr unsigned char MAP_HEIGHT = 21;
+
+//Map width
+constexpr unsigned char MAP_WIDTH = 21;
+
+//Resize value
+constexpr unsigned char SCREEN_RESIZE = 2;
+
+//Cell size
+constexpr unsigned char CELL_SIZE = 16;
+
+//Lettering size
+constexpr unsigned char FONT_HEIGHT = 16;
+
+//Cell ghosts use for chasing
+constexpr unsigned char GHOST_CHASE = 1;
+
+
---
+++ Src/Resources/main.cpp
@@ -0,0 +1,36 @@
+#include <array>
+#include <chrono>
+#include <ctime>
+#include <SFML/Graphics.hpp>
+
+#include "Headers/Globals.hpp"
+
+
+int main()
+{
+    // Game won
+    bool game_won = 0;
+
+    // Framerate independance
+    unsigned lag = 0;
+    std::chrono::time_point<std::chrono::steady_clock> previous_time;
+
+    // Maps
+
+    //SFML events
+    sf::Event event;
+
+    //SFML window 
+    sf::RenderWindow window(sf::VideoMode(CELL_SIZE * MAP_WIDTH * SCREEN_RESIZE, (FONT_HEIGHT + CELL_SIZE * MAP_HEIGHT) * SCREEN_RESIZE), "PMCE", sf::Style::Close);
+    window.setView(sf::View(sf::FloatRect(0, 0, CELL_SIZE * MAP_WIDTH, FONT_HEIGHT + CELL_SIZE * MAP_HEIGHT)));
+
+    //Object instances
+
+    //Game loop 
+    while (window.isOpen())
+    {
+        window.clear();
+        window.display();
+
+    }
+}
